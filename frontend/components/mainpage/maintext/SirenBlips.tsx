9c350f6e4bcecf0dd7d2474ce29f9d437a4eafb8
XbrZey <hibreezo@gmail.com>
2026-07-04 23:26:56 +0545

    added:added new file like netlify.toml and siren effects

---
+++ frontend/components/mainpage/maintext/SirenBlips.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+function SirenBlips() {
+  const redRef = useRef<HTMLDivElement>(null);
+  const blueRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline({ repeat: -1 });
+
+    tl.to(redRef.current, { opacity: 0.8, duration: 0.4, ease: "power1.inOut" })
+      .to(redRef.current, { opacity: 0.2, duration: 0.3, ease: "power1.inOut" }, "+=0.1")
+      .to(blueRef.current, { opacity: 0.8, duration: 0.4, ease: "power1.inOut" }, "<")
+      .to(blueRef.current, { opacity: 0.2, duration: 0.3, ease: "power1.inOut" }, "+=0.1");
+  });
+
+  return (
+    <>
+      <div
+        ref={redRef}
+        className="absolute top-30 left-10 h-[28rem] w-[28rem] rounded-full bg-red-600/20 blur-3xl opacity-20"
+      />
+      <div
+        ref={blueRef}
+        className="absolute top-[30rem] left-10 md:top-[25rem] md:left-[50rem] lg:left-[70rem] h-[28rem] w-[28rem] rounded-full bg-blue-600/20 blur-3xl opacity-20"
+      />
+    </>
+  );
+}
+
+export default SirenBlips;
---
+++ frontend/components/mainpage/maintext/SirenCss.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SirenCss() {
+  return (
+    <main>
+     <div className="absolute top-30 left-10 h-[24rem] w-[28rem] rounded-full bg-red-600/20 blur-3xl opacity-30" />
+     <div className="absolute top-[30rem] left-10 md:top-[25rem] md:left-[50rem] lg:left-[70rem] h-[24rem] w-[28rem] rounded-full bg-blue-600/20 blur-3xl opacity-30" />
+    </main>
+  )
+}
+
+export default SirenCss
